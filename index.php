6c011460d172a8f62c112a56718aad6652055adf
sareytsn <86522797+sareytsn@users.noreply.github.com>
2021-06-26 20:25:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="icon" href="../assets/img/logo/logoga.png">
+<title>Home</title>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link href="../dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="../node_modules/bootstrap-social/bootstrap-social.css">
+
+<!-- Template CSS -->
+<link rel="stylesheet" href="../assets/css/style.css">
+<link rel="stylesheet" href="../assets/css/components.css">
+
+<link href="cover.css" rel="stylesheet">
+</head>
+<body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Gemilang Abadi</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="#">Home</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Hi, Wellcome.</h1>
+        <p class="lead">Untuk masuk ke halaman selanjutnya silahkan pilih kedua button dibawah ini. Have a nice day!</p>
+        <p class="lead">
+          <a href="../pages/login.php" class="btn btn-lg btn-secondary">Sudah punya akun</a>
+          <a href="../pages/register.php" class="btn btn-lg btn-secondary">Belum punya akun</a>
+        </p>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+      </footer>
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+<script src="../assets/js/vendor/popper.min.js"></script>
+<script src="../dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
